e91411f390dd540683ef3a0d4d2707977768fb02
johndebord <i@johndebord.tk>
2018-09-25 01:32:45 -0500

    Fix `defin-key' -> `define-key' typo

--- emacs-modes/major-modes/completion-list-mode/jd:completion-list-mode-keybindings.el
+++ emacs-modes/major-modes/completion-list-mode/jd:completion-list-mode-keybindings.el
@@ -4,9 +4,9 @@
 ;;; `completion-list-mode-map' --- `simple.el'
 (defun jd:completion-list-mode-map ()
   (setf (cdr completion-list-mode-map) nil)
-  (defin-key completion-list-mode-map (kbd "<jd:ret>") 'choose-completion)
-  (defin-key completion-list-mode-map (kbd "<jd:tab>") 'next-completion)
-  (defin-key completion-list-mode-map (kbd "<jd:S-tab>") 'previous-completion)
-  (defin-key completion-list-mode-map (kbd "q") 'quit-window))
+  (define-key completion-list-mode-map (kbd "<jd:ret>") 'choose-completion)
+  (define-key completion-list-mode-map (kbd "<jd:tab>") 'next-completion)
+  (define-key completion-list-mode-map (kbd "<jd:S-tab>") 'previous-completion)
+  (define-key completion-list-mode-map (kbd "q") 'quit-window))
 (add-hook 'completion-list-mode-hook 'jd:completion-list-mode-map)
 
