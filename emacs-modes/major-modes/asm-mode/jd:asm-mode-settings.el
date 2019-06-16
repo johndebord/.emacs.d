9bdba01cde87f623e992a92c0b725ea9277290ce
johndebord <john.debord@block.one>
2019-06-16 18:22:50 -0400

    Add different `as` and `ld` commands for dif arch

--- emacs-modes/major-modes/asm-mode/jd:asm-mode-settings.el
+++ emacs-modes/major-modes/asm-mode/jd:asm-mode-settings.el
@@ -9,9 +9,25 @@
   (setq yas/indent-line nil))
 (add-hook 'asm-mode-hook 'jd:asm-mode-enable-yasnippet-mode)
 
+;; x86_64 --- no dynamic linking
 (defun jd:asm-mode-set-compile-command ()
-  (setq compile-command "as --32 -gstabs -o prog.o prog.s; ld -m elf_i386 --dynamic-linker /lib/ld-linux.so.2 -lc -o prog prog.o; time ./prog"))
+  (setq compile-command "as -gstabs -o prog.o prog.s; ld -o prog prog.o; file prog; time ./prog"))
 (add-hook 'asm-mode-hook 'jd:asm-mode-set-compile-command)
+
+;; x86_64 --- dynamic linking
+;; (defun jd:asm-mode-set-compile-command ()
+;;   (setq compile-command "as -gstabs -o prog.o prog.s; ld -dynamic-linker /lib64/ld-linux-x86-64.so.2 -lc -o prog prog.o; file prog; time ./prog"))
+;; (add-hook 'asm-mode-hook 'jd:asm-mode-set-compile-command)
+
+;; x86_32 --- no dynamic linking
+;; (defun jd:asm-mode-set-compile-command ()
+;;   (setq compile-command "as --32 -gstabs -o prog.o prog.s; ld -m elf_i386 -o prog prog.o; time ./prog"))
+;; (add-hook 'asm-mode-hook 'jd:asm-mode-set-compile-command)
+
+;; x86_32 --- dynamic linking
+;; (defun jd:asm-mode-set-compile-command ()
+;;   (setq compile-command "as --32 -gstabs -o prog.o prog.s; ld -m elf_i386 --dynamic-linker /lib/ld-linux.so.2 -lc -o prog prog.o; time ./prog"))
+;; (add-hook 'asm-mode-hook 'jd:asm-mode-set-compile-command)
 
 (defun jd:asm-mode-set-tab-width ()
   (setq-default tab-width 4))
