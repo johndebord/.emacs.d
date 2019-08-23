d0a84657d00c95c261086a380fc5b3b14afcc0e7
johndebord <john.debord@block.one>
2019-08-23 18:27:53 -0400

    Continuation

--- emacs-modes/third-party-packages/sr-speedbar/jd:sr-speedbar-keybindings.el
+++ emacs-modes/third-party-packages/sr-speedbar/jd:sr-speedbar-keybindings.el
@@ -1,34 +1,30 @@
 ;;; `speedbar-buffers-key-map' --- `speedbar.el'
 (defun jd:speedbar-buffers-key-map ()
   (setf (cdr speedbar-buffers-key-map) nil)
-  (define-key speedbar-file-key-map (kbd "<jd:ret>") 'speedbar-edit-line)
-  (define-key speedbar-file-key-map (kbd "<jd:spc>") 'speedbar-toggle-line-expansion))
+  (define-key speedbar-buffers-key-map (kbd "d") 'speedbar-buffer-kill-buffer)
+  (define-key speedbar-buffers-key-map (kbd "r") 'speedbar-buffer-revert-buffer)
+  (define-key speedbar-buffers-key-map (kbd "<C-i>") 'speedbar-restricted-prev)
+  (define-key speedbar-buffers-key-map (kbd "<C-k>") 'speedbar-restricted-next)
+  (define-key speedbar-buffers-key-map (kbd "<jd:ret>") 'speedbar-edit-line)
+  (define-key speedbar-buffers-key-map (kbd "<jd:spc>") 'speedbar-toggle-line-expansion)
+  (define-key speedbar-buffers-key-map (kbd "<mouse-1>") 'dframe-click)
+  (define-key speedbar-buffers-key-map (kbd "<mouse-3>") 'speedbar-up-directory)
+  (define-key speedbar-buffers-key-map (kbd "<C-mouse-3>") 'dframe-popup-kludge))
 (add-hook 'speedbar-mode-hook 'jd:speedbar-mode-map)
 
 ;;; `speedbar-file-key-map' --- `speedbar.el'
 (defun jd:speedbar-file-key-map ()
   (setf (cdr speedbar-file-key-map) nil)
-
-  (define-key speedbar-file-key-map (kbd "<mouse-1>") 'dframe-click)
-
   (define-key speedbar-file-key-map (kbd "d") 'speedbar-item-delete)
   (define-key speedbar-file-key-map (kbd "i") 'speedbar-item-info)
   (define-key speedbar-file-key-map (kbd "r") 'speedbar-item-rename)
-
-  (define-key speedbar-file-key-map (kbd "<C-k>") 'speedbar-restricted-next)
   (define-key speedbar-file-key-map (kbd "<C-i>") 'speedbar-restricted-prev)
-
-  (define-key speedbar-file-key-map (kbd "<mouse-3>") 'speedbar-up-directory)
-  
-  ;; (define-key speedbar-file-key-map (kbd "4") 'speedbar-edit-line)
-  ;; (define-key speedbar-file-key-map (kbd "5") 'speedbar-edit-line)
-  ;; (define-key speedbar-file-key-map (kbd "6") 'speedbar-edit-line)
-  ;; (define-key speedbar-file-key-map (kbd "7") 'speedbar-edit-line)
-  ;; (define-key speedbar-file-key-map (kbd "8") 'speedbar-edit-line)
-  ;; (define-key speedbar-file-key-map (kbd "9") 'speedbar-edit-line)
-  
+  (define-key speedbar-file-key-map (kbd "<C-k>") 'speedbar-restricted-next)
   (define-key speedbar-file-key-map (kbd "<jd:ret>") 'speedbar-edit-line)
-  (define-key speedbar-file-key-map (kbd "<jd:spc>") 'speedbar-toggle-line-expansion))
+  (define-key speedbar-file-key-map (kbd "<jd:spc>") 'speedbar-toggle-line-expansion)
+  (define-key speedbar-file-key-map (kbd "<mouse-1>") 'dframe-click)
+  (define-key speedbar-file-key-map (kbd "<mouse-3>") 'speedbar-up-directory)
+  (define-key speedbar-file-key-map (kbd "<C-mouse-3>") 'dframe-popup-kludge))
 (add-hook 'speedbar-mode-hook 'jd:speedbar-file-key-map)
 
 ;;; `speedbar-key-map' --- `speedbar.el'
