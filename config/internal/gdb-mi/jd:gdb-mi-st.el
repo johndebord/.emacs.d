e06e2b73a17d3ffdb0e38e4138ea677ca24a9ad4
johndebord <i@johndebord.tk>
2019-11-02 17:25:48 -0400

    Update

--- config/internal/gdb-mi/jd:gdb-mi-st.el
+++ config/internal/gdb-mi/jd:gdb-mi-st.el
@@ -2,28 +2,9 @@
 (setq-default gdb-many-windows t)
 (setq-default gdb-show-main nil)
 
-;; ;;;; Window management
-;; (defun jd:gdb-display-buffer (buf)
-;;   "Show buffer BUF, and make that window dedicated."
-;;   (let ((window (display-buffer buf)))
-;;     window))
-
-;; (advice-add 'gdb-display-buffer :override 'jd:gdb-display-buffer)
-
-;; GDBC
-;; BRKP
-;; LOCL
-;; REGI
-;; STCK
-;; THRD
-;; DASM
-;; MEMO
-;; INIO
-;; PRIO
-
 ;; (defconst jd:gdb-comint-header
 ;;   (list
-;;    ;; (gdb-propertize-header "GDB"         gdb-comint-buffer        nil nil                 mode-line)          " "
+;;    (gdb-propertize-header "GDB"         gdb-comint-buffer        nil nil                 mode-line)          " "
 ;;    (gdb-propertize-header "Breakpoints" gdb-breakpoints-buffer   nil mode-line-highlight mode-line-inactive) " "
 ;;    (gdb-propertize-header "Locals"      gdb-locals-buffer        nil mode-line-highlight mode-line-inactive) " "
 ;;    (gdb-propertize-header "Registers"   gdb-registers-buffer     nil mode-line-highlight mode-line-inactive) " "
@@ -119,37 +100,15 @@
 
 (defconst jd:gdb-io-header
   (list
-   ;; (gdb-propertize-header "GDB"         gud-comint-buffer      nil mode-line-highlight mode-line-inactive)  " "
-   (gdb-propertize-header "Breakpoints" gdb-breakpoints-buffer nil mode-line-highlight mode-line-inactive)  " "
-   (gdb-propertize-header "Locals"      gdb-locals-buffer      nil mode-line-highlight mode-line-inactive)  " "
-   (gdb-propertize-header "Registers"   gdb-registers-buffer   nil mode-line-highlight mode-line-inactive)  " "
-   (gdb-propertize-header "Stack"       gdb-stack-buffer       nil mode-line-highlight mode-line-inactive)  " "
-   (gdb-propertize-header "Threads"     gdb-threads-buffer     nil mode-line-highlight mode-line-inactive)  " "
-   (gdb-propertize-header "Disassembly" gdb-disassembly-buffer nil mode-line-highlight mode-line-inactive)  " "
+   ;; (gdb-propertize-header "GDB"         gud-comint-buffer      nil mode-line-highlight mode-line-inactive) " "
+   (gdb-propertize-header "Breakpoints" gdb-breakpoints-buffer nil mode-line-highlight mode-line-inactive) " "
+   (gdb-propertize-header "Locals"      gdb-locals-buffer      nil mode-line-highlight mode-line-inactive) " "
+   (gdb-propertize-header "Registers"   gdb-registers-buffer   nil mode-line-highlight mode-line-inactive) " "
+   (gdb-propertize-header "Stack"       gdb-stack-buffer       nil mode-line-highlight mode-line-inactive) " "
+   (gdb-propertize-header "Threads"     gdb-threads-buffer     nil mode-line-highlight mode-line-inactive) " "
+   (gdb-propertize-header "Disassembly" gdb-disassembly-buffer nil mode-line-highlight mode-line-inactive) " "
    (gdb-propertize-header "Memory"      gdb-memory-buffer      nil mode-line-highlight mode-line-inactive) " "
    (gdb-propertize-header "Inferior IO" gdb-inferior-io        nil nil                 mode-line)))
-
-;; NOTES:
-;; (gdb-init-1) is called from (gdb-update)
-;; (gdb-get-buffer-create 'gdb-inferior-io) is called from (gdb-init-1)
-;; (gdb-get-buffer-create 'gdb-threads-buffer) and (gdb-get-buffer-create 'gdb-breakpoints-buffer)
-;; get executed in (gdb-update)
-;; (gdb-get-buffer-create 'gdb-locals-buffer) and
-;; (gdb-get-buffer-create 'gdb-stack-buffer)  and
-;; (gdb-get-buffer-create 'gdb-breakpoints-buffer)
-;; get executed in (gdb-setup-windows)
-;;
-;; (gdb-display-buffer-other-frame-action) is the function that displays the other thread
-;;
-;; IO has a few useful functions: (gdb-io-interrupt) (gdb-io-quit) (gdb-io-stop) (gdb-io-eof)
-;; IO buffer is init in (gdb-init-1)
-;; THREADS and BREAKPOINTS bufs are init in (gdb-update)
-;; REGISTER is called from (gdb-update) -> (gdb-get-changed-registers)
-;;
-;; the variables gdb-thread-number shoudl be passed as the thread argument
-;; it's the thread number of the main thread
-;;
-;; I have an idea to prevent popup-frames; replace the frame popup symbol with a goto buffer like symbol?
 
 (defun jd:gdb-startup ()
   (sleep-for 1)
@@ -158,45 +117,25 @@
     (set-window-buffer win0 gud-comint-buffer)
     (if (gud-find-file gdb-main-file)
         (progn
-          (let ((win1 (split-window)))
+          (let ((win1 (split-window))
+                (win2 (split-window)))
             (set-window-buffer win1 (gud-find-file gdb-main-file))
+            (set-window-buffer win2 (gdb-inferior-io-name))
             (select-window win1)
-            (setq gdb-source-window (selected-window)))))
-    (select-window win0)
-    ;; (gdb-get-buffer-create 'gdb-breakpoints-buffer)
-    ;; (gdb-get-buffer-create 'gdb-locals-buffer)
-    ;; (gdb-get-buffer-create 'gdb-registers-buffer)
-    ;; (gdb-get-buffer-create 'gdb-stack-buffer)
-    ;; (gdb-get-buffer-create 'gdb-threads-buffer)
-    ;; (gdb-get-buffer-create 'gdb-disassembly-buffer)
-    ;; (gdb-get-buffer-create 'gdb-memory-buffer)
-    ;; (gdb-get-buffer-create 'gdb-inferior-io)
-    ;; (with-current-buffer (gdb-breakpoints-buffer-name)
-    ;;   (setq-local header-line-format 'jd:gdb-breakpoints-header))
-    ;; (with-current-buffer (gdb-locals-buffer-name)
-    ;;   (setq-local header-line-format 'jd:gdb-locals-header))
-    ;; (with-current-buffer (gdb-registers-buffer-name)
-    ;;   (setq-local header-line-format 'jd:gdb-registers-header))
-    ;; (with-current-buffer (gdb-stack-buffer-name)
-    ;;   (setq-local header-line-format 'jd:gdb-stack-header))
-    ;; (with-current-buffer (gdb-threads-buffer-name)
-    ;;   (setq-local header-line-format 'jd:gdb-threads-header))
-    ;; (with-current-buffer (gdb-disassembly-buffer-name)
-    ;;   (setq-local header-line-format 'jd:gdb-disassembly-header))
-    ;; (with-current-buffer (gdb-memory-buffer-name)
-    ;;   (setq-local header-line-format 'jd:gdb-memory-header))
-    ;; (with-current-buffer (gdb-inferior-io-name)
-    ;;   (setq-local header-line-format 'jd:gdb-io-header))
-    ))
+            (setq gdb-source-window (selected-window))))
+      (progn
+        (let ((win1 (split-window)))
+          (set-window-buffer win1 (gdb-inferior-io-name)))))
+    (select-window win0)))
 
-;; (defun jd:gdb-shutdown ()
-;;   (sleep-for 1)
-;;   (delete-other-windows)
-;;   (eshell))
+(defun jd:gdb-shutdown ()
+  (sleep-for 1)
+  (delete-other-windows)
+  (eshell))
 
 (add-hook 'gdb-mode-hook 'jd:gdb-startup)
-;; (add-hook 'gud-kill-buffer-hook 'jd:gdb-shutdown)
-;; gud-kill-buffer-hook
+
+(advice-add 'gud-kill-buffer-hook :after 'jd:gdb-shutdown)
 
 ;; (defun jd:gdb-setup-windows ()
 ;;   ;; (gdb-get-buffer-create 'gud-comint-buffer)
@@ -237,7 +176,26 @@
 ;;             (setq gdb-source-window (selected-window)))))
 ;;     (select-window win0)))
 
-
-;; (advice-add 'gdb-setup-windows :override 'jd:gdb-setup-windows)
+;; NOTES:
+;; (gdb-init-1) is called from (gdb-update)
+;; (gdb-get-buffer-create 'gdb-inferior-io) is called from (gdb-init-1)
+;; (gdb-get-buffer-create 'gdb-threads-buffer) and (gdb-get-buffer-create 'gdb-breakpoints-buffer)
+;; get executed in (gdb-update)
+;; (gdb-get-buffer-create 'gdb-locals-buffer) and
+;; (gdb-get-buffer-create 'gdb-stack-buffer)  and
+;; (gdb-get-buffer-create 'gdb-breakpoints-buffer)
+;; get executed in (gdb-setup-windows)
+;;
+;; (gdb-display-buffer-other-frame-action) is the function that displays the other thread
+;;
+;; IO has a few useful functions: (gdb-io-interrupt) (gdb-io-quit) (gdb-io-stop) (gdb-io-eof)
+;; IO buffer is init in (gdb-init-1)
+;; THREADS and BREAKPOINTS bufs are init in (gdb-update)
+;; REGISTER is called from (gdb-update) -> (gdb-get-changed-registers)
+;;
+;; the variables gdb-thread-number shoudl be passed as the thread argument
+;; it's the thread number of the main thread
+;;
+;; I have an idea to prevent popup-frames; replace the frame popup symbol with a goto buffer like symbol?
 
 (provide 'jd:gdb-mi-st.el)
