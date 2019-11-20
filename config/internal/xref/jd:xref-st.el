b00b11143ee610091ec1d6d41c8351020281aaba
johndebord <i@johndebord.tk>
2019-11-20 03:09:18 -0500

    Fix xref properties

--- config/internal/xref/jd:xref-st.el
+++ config/internal/xref/jd:xref-st.el
@@ -1,9 +1,12 @@
 (setq-default xref-after-jump-hook nil)
 
-;; (defun xref--insert-propertized (props &rest strings))
-
-;; (advice-add 'xref--insert-propertized :override (lambda (props &rest strings)))
-
+(defun jd:xref--insert-propertized (props &rest strings)
+  (let ((start (point)))
+    (apply #'insert strings)
+    (add-text-properties start (point) props)
+    (if (plist-member (text-properties-at start) 'xref-item)
+        (remove-text-properties start (point) '(help-echo nil face nil)))))
+  
 (defun jd:xref-next-line ()
   (interactive)
   (beginning-of-line)
@@ -21,5 +24,7 @@
               (backward-sexp)
               (recenter)
               (xref-pulse-momentarily))))
+
+(advice-add 'xref--insert-propertized :override 'jd:xref--insert-propertized)
 
 (provide 'jd:xref-st.el)
